3aea55d5ded7d8f0ea66ee05a2b0f106943cdaa1
Kost Kikinov <kikinov.k@gmail.com>
2021-08-30 00:01:18 +0300

    feat: test 3 of 7

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,60 @@
+//
 class TicTacToe {
-    constructor() {
 
+    constructor() {
+        this._playerSymbols = ['x','o'];
+        this.currentPlayerSymbol = this._playerSymbols[0];
+        this.playingField = Array.from(Array(3), () => Array(3).fill(null));
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayerSymbol
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.playingField[rowIndex][columnIndex] === null) {
+            this.playingField[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+            this.currentPlayerSymbol = this._playerSymbols.reverse()[0]
+        }
     }
 
     isFinished() {
-
+        return !!this.getWinner() || this.isDraw()
     }
 
     getWinner() {
+        let result = [];
+        let mainDiagonal = [];
+        let minorDiagonal = [];
+        for(let i = 0; i < this.playingField.length; i++) {
+            let column = [];
+            for(let j = 0; j < this.playingField[i].length; j++){
+                column.push(this.playingField[j][i]);
+                if (i === j) {
+                    mainDiagonal.push(this.playingField[i][j])
+                };
+                if (i === this.playingField.length - j - 1) {
+                    minorDiagonal.push(this.playingField[i][this.playingField[i].length - j - 1])
+                }
+            };
+            result.push(this.playingField[i], column);
+        };
+        result.push(mainDiagonal, minorDiagonal);
+        let resultSymbol = result.filter(array => new Set(array).size == 1 && array[0])
+        return resultSymbol.length > 1 ? resultSymbol[0][0] : null
 
     }
 
     noMoreTurns() {
-
+        return this.playingField.every(array => array.every(elem => elem !== null))
     }
 
     isDraw() {
-
+        return !this.getWinner() && this.noMoreTurns()
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.playingField[rowIndex][colIndex]
     }
 }
 
